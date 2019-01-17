a2a0fa96838f03e7e418894ab30214cf3095efc7
Tanseer Saji <tanseersaji@gmail.com>
2019-01-17 21:46:27 +0530

    updated code

--- EyeBlinking.py
+++ EyeBlinking.py
@@ -24,7 +24,7 @@ def response(args):
         cv2.drawContours(frame, [np.asarray(leftEyeHull)], -1, (0, 255, 0), 1)
         cv2.drawContours(frame, [np.asarray(rightEyeHull)], -1, (0, 255, 0), 1)
         if blinking ==1:
-            blinkingDetect=1
+            blinkingDetect += 1
             
 def eye_blinking_video():
     vidcap = cv2.VideoCapture(0)
@@ -46,7 +46,7 @@ def eye_blinking_video():
             vidcap.release()
             cv2.destroyAllWindows()
             break
-        if blinkingDetect==1:
+        if blinkingDetect == 3:
             vidcap.release()
             cv2.destroyAllWindows()
             send_Video_face_search()
@@ -54,8 +54,8 @@ def eye_blinking_video():
     
 socketio2 = SocketIO('http://socket.giscle.com', 8000, LoggingNamespace)
 
-token ="YOUR_API_KEY"
-secret ="YOUR_SECRET_KEY"
+token = "YOUR_API_KEY"
+secret = "YOUR_SECRET_KEY"
 folder = "default"
 def response2(args):
     print('response', str(args['data']))
